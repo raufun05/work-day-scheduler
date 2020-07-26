96f44d3e1872c5b53f72d75036243f4f598543d7
Raufun <raufun05@yahoo.com>
2020-07-25 19:27:38 -0600

    Set Items and Get Items from local storage

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 //The ready() method is used to make a function available after the document is loaded.
 $(document).ready(function () {
     //Used Moment.js library to display current day & time 
-    $('#currentDay').text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm:ss a'));
-      
+    $('#currentDay').text("Today is " + moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm:ss a'));
+    //$("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); 
     //Assign saveBtn click listener for user input and time stamp??
-    $(".saveBtn").on("click", function () {
-        //get nearby values.
+    // $(".saveBtn").on("click", function () {
+    //     //get nearby values.
+    //     console.log(this);
+    //     var text = $(this).siblings(".description").val();
+    //     var time = $(this).parent().attr("id");
+
+        
+        //localStorage.setItem(time, text);
+    //Assign saveBtn click listener for user input and time stamp??   
+        $("button").click(function() {
+           //get nearby values.
         console.log(this);
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
-
-        //set items in local storage.
+          //set items in local storage.  
         localStorage.setItem(time, text);
-        // $("button").click(function() {
-        //    //get nearby values.
-        // console.log(this);
-        // var text = $(this).siblings(".description").val();
-        // var time = $(this).parent().attr("id");
-            
-        // localStorage.setItem(time, text);
          
     })
     //load any saved data from LocalStorage - do this for each hour created.
-    $("#nine-am .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#time9am .description").val(localStorage.getItem("time9am"));
+    $("#time10am .description").val(localStorage.getItem("time10am"));
+    $("#time11am .description").val(localStorage.getItem("time11am"));
+    $("#time12pm .description").val(localStorage.getItem("time12pm"));
+    $("#time13pm .description").val(localStorage.getItem("time13pm"));
+    $("#time14pm .description").val(localStorage.getItem("time14pm"));
+    $("#time15pm .description").val(localStorage.getItem("time15pm"));
+    $("#time16pm .description").val(localStorage.getItem("time16pm"));
+    $("#time17pm .description").val(localStorage.getItem("time17pm"));
 
 
     function hourTracker() {
@@ -39,7 +40,7 @@ $(document).ready(function () {
 
         // loop over time blocks
         $(".time-block").each(function () {
-            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            var blockHour = parseInt($(this).attr("id").split("time")[1]);
             console.log( blockHour, currentHour)
 
             //check if we've moved past this time
